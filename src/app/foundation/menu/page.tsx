07417a9ecb8f08a8ceea03402af9cd785136de78
Takagi Tasuku <tasuren@outlook.jp>
2023-06-19 07:04:15 +0900

    めにゅーの作りかけ。

---
+++ src/app/foundation/menu/page.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+
+function ServerSelect() {
+  const [serverId, setServer] = useState(0);
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+
+export default function Menu() {
+  return (
+    <div>
+      <ServerSelect></ServerSelect>
+      <hr />
+    </div>
+  );
+};
